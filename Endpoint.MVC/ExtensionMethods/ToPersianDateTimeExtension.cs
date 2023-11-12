31251d545f6aca922aeddfd12b7f844ed0526b86
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-11-12 20:22:15 +0330

    add extension method for convert datetime to persian

---
+++ Endpoint.MVC/ExtensionMethods/ToPersianDateTimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Endpoint.MVC.ExtensionMethods;
+
+public static class ToPersianDateTimeExtension
+{
+    public static string ToPersian(this DateTime dateTime)
+    {
+        var persianCalendar = new PersianCalendar();
+
+        return String.Format("{0}/{1}/{2}", 
+                             persianCalendar.GetYear(dateTime),
+                             persianCalendar.GetMonth(dateTime),
+                             persianCalendar.GetDayOfMonth(dateTime));
+    }
+}
